8ce9acadece4c9672d84b5a30d9dc7d6b216f1c1
umarNemon <107072941+umarNemon@users.noreply.github.com>
2022-12-19 15:05:25 +0600

    News pages Product

--- src/componens/news/News.jsx
+++ src/componens/news/News.jsx
@@ -1,11 +1,13 @@
 import Header from "../Layout/Header/Header";
 import Footer from "../Layout/Footer/Footer";
 import NewsTitle from "./NewsTitle/NewsTitle";
+import NewsContent from "./NewsContent/NewsContent";
 const News = () =>{
     return(
         <div>
         <Header  bg={'linear-gradient(285.45deg, #0B3F37 38.27%, #CB8D62 141.81%)'}/>
         <NewsTitle/>
+        <NewsContent/>
        <Footer/>
         </div>
     )
